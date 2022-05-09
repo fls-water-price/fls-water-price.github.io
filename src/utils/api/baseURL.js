410e4227d992052d61c792a583684eda46aac048
Partheeban Bharati <b.partheeban@hotmail.com>
2022-05-08 17:01:19 -0700

    changed base url to aws

--- src/utils/api/baseURL.js
+++ src/utils/api/baseURL.js
@@ -1,4 +1,4 @@
 export const baseURL =
   process.env.NODE_ENV === "production"
-    ? "/api/v1"
+    ? "http://ec2-52-90-156-242.compute-1.amazonaws.com/api/v1"
     : "http://localhost:3001/api/v1";
